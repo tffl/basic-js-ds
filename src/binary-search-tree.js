93a91401afa5dba4012c5375aff8d1ba3680010f
Natalya M <natalyamerr@gmail.com>
2025-01-06 16:52:34 +0300

    feat: implement binary search tree root and add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,39 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor(data) {
+    data ? this.rootNode = new Node(data) : this.rootNode = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let curr = null;
+    if (!this.rootNode) {
+      this.rootNode = new Node(data);
+    } else {
+      curr = this.rootNode
+      while (curr.left || curr.right) {
+        if (data < curr.data) {
+          if (!curr.left) {
+            curr.left = new Node(data);
+            return;
+          } else { curr = curr.left }
+        } else if (data > curr.data) {
+          if (!curr.right) {
+            curr.right = new Node(data);
+            return;
+          } else { curr = curr.right }
+        }
+      }
+    }
   }
 
   has(/* data */) {
